6a038aed84a6364e6e6dbe31a8f097d56a1312a0
jiduhutoxayerude <37327360+jiduhutoxayerude@users.noreply.github.com>
2018-03-13 09:43:51 +0100

    Add Category file

---
+++ category.sql
@@ -0,0 +1,19 @@
+USE internal;
+
+
+INSERT INTO category (id, name, blacklisted) VALUES
+  (1, 'adult', 0),
+  (2, 'bank', 0),
+  (3, 'blog', 0),
+  (4, 'dating', 0),
+  (5, 'filehosting', 0),
+  (6, 'gambling', 0),
+  (7, 'game', 0),
+  (8, 'malware', 0),
+  (9, 'manga', 0),
+  (10, 'phishing', 0),
+  (11, 'pub', 0),
+  (12, 'shopping', 0),
+  (13, 'socialnetwork', 0),
+  (14, 'sport', 0),
+  (15, 'webmail', 0);
